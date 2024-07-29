4b69e0523e3048ad0acddd493befce38fa32b587
Kihwan <eljsh95@naver.com>
2024-07-29 22:04:21 +0900

    vue

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+    transpileDependencies: true,
+    css: {
+        loaderOptions: {
+            scss: {
+                additionalData: `
+          @import "@/assets/scss/base/_index.scss";
+          @import "@/assets/scss/helpers/_index.scss";
+          @import "@/assets/scss/layout/_index.scss";
+        `,
+            },
+        },
+    },
+    publicPath: process.env.BASE_URL,
+    assetsDir: process.env.BASE_URL,
+    devServer: {
+        // contentBase를 사용하지 않고 static을 사용합니다.
+        static: {
+            directory: path.join(__dirname, ''), // 설정하면 url(/src/assets") 경로 사용 가능
+        },
+    },
+});
